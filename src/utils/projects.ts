aa63deb6fb4649bd460b551c4494ad034f95889f
Jason Aravanis <j.aravanis@icloud.com>
2022-03-22 14:38:45 +1100

    Add more projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -3,6 +3,9 @@ import skills from "./skills";
 import quicksnapGIF from "../images/gifs/quicksnap.gif";
 import fakebookGIF from "../images/gifs/fakebook.gif";
 import wheresWaldoGIF from "../images/gifs/wheresWaldo.gif";
+import shoppingCartGIF from "../images/gifs/shoppingCart.gif";
+import resumeBuilderGIF from "../images/gifs/resumeBuilder.gif";
+import weatherGIF from "../images/gifs/weather.gif";
 
 const projects: IProject[] = [
   {
@@ -51,6 +54,33 @@ const projects: IProject[] = [
       skills.styledComponents,
       skills.jest,
     ],
+  },
+  {
+    title: "Shopping Cart",
+    image: shoppingCartGIF,
+    url: "https://atlas-1510.github.io/shopping-cart",
+    repo: "https://github.com/Atlas-1510/shopping-cart",
+    description:
+      "This is a mock up of a shopping cart web application, as part of The Odin Project syllabus. Images and inspiration come from the Knights of Suburbia cycling website.",
+    builtWith: [skills.react, skills.styledComponents, skills.reactSpring],
+  },
+  {
+    title: "Resume Builder",
+    image: resumeBuilderGIF,
+    url: "https://atlas-1510.github.io/cv-project/",
+    repo: "https://github.com/Atlas-1510/cv-project",
+    description:
+      "This React application creates a formatted resume based on user input of personal details, experience, and education history. The resume can be printed or saved as a PDF.",
+    builtWith: [skills.react, skills.styledComponents],
+  },
+  {
+    title: "Weather Search",
+    image: weatherGIF,
+    url: "https://atlas-1510.github.io/weather-app/",
+    repo: "https://github.com/Atlas-1510/weather-app",
+    description:
+      "This application allows a user to view weather and local time for their current location, or a custom location. Units can be changed from metric to imperial. The project use the Nominatim API for latitude/longitude lookup, and OpenWeatherMap API to get weather data for those coordinates. The geolocation API is used to get weather at user location.",
+    builtWith: [skills.sass, skills.webpack, skills.npm, skills.git],
   },
 ];
 
