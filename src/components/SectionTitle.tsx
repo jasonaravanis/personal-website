cbbfa16a70ebeaac157c860964a2ed1edefc0c5d
Jason Aravanis <j.aravanis@icloud.com>
2022-03-23 15:35:03 +1100

    Make section titles respond to dark mode

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,6 +1,23 @@
+import { ReactNode, FC } from "react";
 import styled from "styled-components";
+import { useTheme } from "../contexts/Theme";
+import DarkMode from "../interfaces/DarkMode";
+import colours from "../styles/colours";
 
-export const SectionTitle = styled.h1`
+interface Props {
+  children: ReactNode;
+}
+
+const Container = styled.h1<DarkMode>`
   font-size: 3rem;
   margin: 1rem 0 1rem 0;
+  font-family: "Tungsten";
+  text-transform: uppercase;
+  color: ${(props) =>
+    props.darkMode ? colours.dark.primary : colours.light.primary};
 `;
+
+export const SectionTitle: FC<Props> = ({ children }) => {
+  const darkMode = useTheme();
+  return <Container darkMode={darkMode}>{children}</Container>;
+};
